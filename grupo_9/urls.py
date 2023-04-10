9407e2367d2e82290645ba12ce12dfc77928b808
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-10 10:20:34 -0300

    included fines.urls

--- grupo_9/urls.py
+++ grupo_9/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('fines.urls'))
+    path('fines/', include('fines.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
