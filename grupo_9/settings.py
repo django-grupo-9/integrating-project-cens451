876f2306c11d3576fa0adc56a97af3fdff575922
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-10 11:25:18 -0300

    added staticfiles_dirs config to load static

--- grupo_9/settings.py
+++ grupo_9/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ["static/"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
