848576536343b56286cecda2e7ef372ea37c664b
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-10 10:31:24 -0300

    path home y docentes for temporal solution

--- fines/urls.py
+++ fines/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.fines, name="fines"),
+    # Solución temporal para error en navbar!
+    path('/home', views.home, name='home'),
+    path('/docentes', views.docentes, name='docentes'),
 ]
