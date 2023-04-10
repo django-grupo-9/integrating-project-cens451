a4cdb2388b49b724aed31483668b196011121fdb
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-10 10:19:51 -0300

    fines view to render fines.html

--- fines/views.py
+++ fines/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html", {"title": "CENTRO EDUCATIVO DE NIVEL SECUNDARIO N° 451"})
+
+
+def fines(request):
+    return render(request, "fines.html", {"title": "Plan Fines"})
