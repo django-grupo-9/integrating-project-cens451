993d5b73fe32c58d7b2192b0c13c6415c80e98cd
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-11 12:22:38 -0300

    deleted 'home' path

--- fines/urls.py
+++ fines/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.fines, name="fines"),
     # Solución temporal para error en navbar!
-    path('home/', views.home, name='home'),
     path('docentes/', views.docentes, name='docentes'),
 ]
