2159905a59b4ec4f9655e19320fc02183a479f9f
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-11 11:22:59 -0300

    changed empty route to "index"

--- fines/urls.py
+++ fines/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.fines, name="fines"),
-    path('index/', views.index, name='index'),
     # Solución temporal para error en navbar!
     path('home/', views.home, name='home'),
     path('docentes/', views.docentes, name='docentes'),
--- grupo_9/urls.py
+++ grupo_9/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from fines import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', v.index, name='index'),
     path('fines/', include('fines.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
