68b3dc175e63860b27312d3e67b632f069159aac
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-11 12:23:10 -0300

    fix

--- grupo_9/urls.py
+++ grupo_9/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
-from fines import views as v
+from fines import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', v.index, name='index'),
+    path('', views.index, name='index'),
     path('fines/', include('fines.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
