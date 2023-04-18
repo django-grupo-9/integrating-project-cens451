4b12d355e75498de43b608e45201b96437e0f7b8
abellamas <abellamasm@gmail.com>
2023-04-18 11:01:13 -0300

    Nuevo arreglo en docentes

--- grupo_9/views.py
+++ grupo_9/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, "pages/index.html", {"title": "CENTRO EDUCATIVO DE NIVEL SECUNDARIO N° 451"})
 
 def docentes(request):
-    return HttpResponse("<h1>Solución temporal!!</h1>")
+    return render(request, "pages/docentes.html", {"title": "DOCENTES"})
 
 def contacto(request):
     return render(request, 'pages/contacto.html',{"title": "CONTACTO"})
