2816c20dbb5ce40b53fd0c2e5a559d85c7a218e0
Osvaldo Meijide Sistemas <sistemas.om@gmail.com>
2023-04-18 08:16:59 -0300

    con vista parametrizada
    Vista parametrizadas con re_path
    Da error (faltará librería de re_path?)

--- fines/urls.py
+++ fines/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.fines, name="fines"),
     # Solución temporal para error en navbar!
     path('docentes/', views.docentes, name='docentes'),
+    # Vista parametrizada inicial, busqueda de materias en carreras
+    # re_path(r'^(?P<carrera>[a-zA-Z_]{1,25})/(?P<curso>[a-zA-Z-0-9_]{1,25})/$', views.materias, name='materias'),
 ]
