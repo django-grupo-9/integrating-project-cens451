73597deb1b0037c9303fe358896e591218f6a841
abellamas <abellamasm@gmail.com>
2023-04-18 10:39:45 -0300

    Merge branch 'origin/dev'

--- fines/urls.py
+++ fines/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.urls import re_path
+
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,6 @@ urlpatterns = [
     path('contacto', views.contacto, name='contacto'),
     # Solución temporal para error en navbar!
     path('docentes/', views.docentes, name='docentes'),
+    # Vista parametrizada inicial, busqueda de materias en carreras
+    re_path(r'^(?P<carrera>[a-zA-Z_]{1,25})/(?P<curso>[a-zA-Z-0-9_]{1,25})/$', views.materias, name='materias'),
 ]
