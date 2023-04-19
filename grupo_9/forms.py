76ac1fa80d7be7a27f0e77ce3f547905fd76d7a9
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-04-19 12:57:33 -0300

    creada form class para registro (falta validacion)

--- grupo_9/forms.py
+++ grupo_9/forms.py
@@ -40,3 +40,57 @@ class LoginForm(forms.Form):
         if not any(c.isdigit() for c in password):
             raise ValidationError('La contraseña debe tener al menos un número')
         return password
+
+
+class SignUpForm(forms.Form):
+    user = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'id': 'user',
+                'class': 'input',
+                'placeholder': 'Usuario',
+                'minlength': '8',
+                'required': True,
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'id': 'email',
+                'class': 'input',
+                'placeholder': 'Email',
+                'required': True,
+                'pattern': '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$',
+                'title': 'Por favor, ingrese un correo electrónico válido (ejemplo@dominio.com)',
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'id': 'password',
+                'class': 'input',
+                'placeholder': 'Contraseña',
+                'required': True,
+                'pattern': '(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}',
+                'title': 'La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.',
+            }
+        )
+    )
+
+    pass_repeat = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'id': 'pass',
+                'class': 'input',
+                'placeholder': 'Repetir contraseña',
+                'required': True,
+                'pattern': '(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}',
+                'title': 'La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.',
+            }
+        )
+    )
+
