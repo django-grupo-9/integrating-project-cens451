d385b00bf2832768ecb61a3c5bcc91cfba5c0f10
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-02 17:07:07 -0300

    Settings para mandar mails

--- grupo_9/settings.py
+++ grupo_9/settings.py
@@ -143,3 +143,13 @@ STATICFILES_DIRS = ["static/"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+env = environ.Env()
+environ.Env.read_env()
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
