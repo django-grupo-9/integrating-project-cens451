fb3a0fc78db26c49868592abed1378da2c437d20
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-17 17:35:06 -0300

    modelos para futura administracion docente

--- cens/models.py
+++ cens/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from fines.models import Persona
 # Create your models here.
+
+
+class Professor(Persona):
+    cuil = models.CharField(max_length=10, blank=False, unique=True, verbose_name="CUIL")
+
+    class Meta:
+        verbose_name = 'Profesor'
+        verbose_name_plural = 'Profesores'
