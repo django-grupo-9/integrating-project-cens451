64d338cfb5c7709ce7f72ad1e57cf1f01453d8e4
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-22 14:54:05 -0300

    javascript que retira los required cuando se checkea que fue alumno

--- static/js/preinscripcion.js
+++ static/js/preinscripcion.js
@@ -1,11 +1,26 @@
 function CheckboxChange() {
     var checkbox = document.getElementById('id_ex_alumno');
     var changeDisplayDiv = document.getElementById('change_display');
+    var estudios = document.getElementById('id_estudios');
+    var colegio = document.getElementById('id_colegio');
+    var pais = document.getElementById('id_pais');
+    var provincia = document.getElementById('id_provinciaColegio');
+    var localidad = document.getElementById('id_localidad');
     
     if (checkbox.checked) {
       changeDisplayDiv.style.display = 'none';
+      estudios.required = false;
+      colegio.required = false;
+      pais.required = false;
+      provincia.required = false;
+      localidad.required = false;
     } else {
       changeDisplayDiv.style.display = 'block';
+      estudios.required = true;
+      colegio.required = true;
+      pais.required = true;
+      provincia.required = true;
+      localidad.required = true;
     }
   }
 
