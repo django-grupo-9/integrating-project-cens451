c5323de0ed309ed67bf699a0ede2b7a6de32f0c2
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-23 15:48:23 -0300

    pycodestyle changes

--- cens/views.py
+++ cens/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def cens(request):
-    
-    return render(request, "pages/cens.html", context={"title":"Secundario CENS"})
+
+    return render(request, "pages/cens.html", context={"title": "Secundario CENS"})
