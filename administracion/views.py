3cfedb280d5aab24b423c2ca57d59503ba1b5925
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-24 13:48:38 -0300

    Views para CRUD orientacion

--- administracion/views.py
+++ administracion/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from administracion.forms import OrientacionForm, CampusForm, AsignaturaForm, ComisionForm
 
-from administracion.models import Categoria, Curso, Orientacion, Comision, Asignatura, Campus
+from administracion.models import Orientacion, Comision, Asignatura, Campus
 
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -33,7 +33,7 @@ def orientacion_nuevo(request):
             return redirect('orientacion_index')
     else:
         formulario = OrientacionForm()
-    return render(request, 'administracion/crud/nuevo.html', {'form': formulario})
+    return render(request, 'administracion/crud/new.html', {'form': formulario})
 
 
 def orientacion_editar(request, id_orientacion):
@@ -49,60 +49,60 @@ def orientacion_editar(request, id_orientacion):
             return redirect('orientacion_index')
     else:
         formulario = OrientacionForm(instance=orientacion)
-    return render(request, 'administracion/crud/editar.html', {'form': formulario})
+    return render(request, 'administracion/crud/edit.html', {'form': formulario})
 
 
 def orientacion_eliminar(request, id_orientacion):
     try:
         orientacion = Orientacion.objects.get(pk=id_orientacion)
-    except Categoria.DoesNotExist:
+    except Orientacion.DoesNotExist:
         return render(request, 'administracion/404_admin.html')
     orientacion.soft_delete()
     return redirect('orientacion_index')
 
 
 # IMPLEMENTACION DE CRUD DE CATEGORIA POR MEDIO DE VISTAS BASADAS EN CLASES (VBC)
-class CategoriaListView(ListView):
-    model = Categoria
-    context_object_name = 'categorias'
-    template_name = 'administracion/crud/index.html'
-    queryset = Categoria.objects.filter(baja=False)
-    ordering = ['nombre']
+# class CategoriaListView(ListView):
+#     model = Categoria
+#     context_object_name = 'categorias'
+#     template_name = 'administracion/crud/index.html'
+#     queryset = Categoria.objects.filter(baja=False)
+#     ordering = ['nombre']
 
 
-class CategoriaCreateView(CreateView):
-    model = Categoria
-    fields = ['nombre']
-    # form_class = CategoriaForm
-    template_name = 'administracion/crud/nuevo.html'
-    success_url = reverse_lazy('categorias_index')
+# class CategoriaCreateView(CreateView):
+#     model = Categoria
+#     fields = ['nombre']
+#     # form_class = CategoriaForm
+#     template_name = 'administracion/crud/nuevo.html'
+#     success_url = reverse_lazy('categorias_index')
 
 
-class CategoriaUpdateView(UpdateView):
-    model = Categoria
-    fields = ['nombre']
-    # form_class = CategoriaForm
-    template_name = 'administracion/crud/editar.html'
-    success_url = reverse_lazy('categorias_index')
+# class CategoriaUpdateView(UpdateView):
+#     model = Categoria
+#     fields = ['nombre']
+#     # form_class = CategoriaForm
+#     template_name = 'administracion/crud/editar.html'
+#     success_url = reverse_lazy('categorias_index')
 
-    def get_object(self, queryset=None):
-        pk = self.kwargs.get(self.pk_url_kwarg)
-        obj = get_object_or_404(Categoria, pk=pk)
-        return obj
+#     def get_object(self, queryset=None):
+#         pk = self.kwargs.get(self.pk_url_kwarg)
+#         obj = get_object_or_404(Categoria, pk=pk)
+#         return obj
 
 
-class CategoriaDeleteView(DeleteView):
-    model = Categoria
-    template_name = 'administracion/crud/eliminar.html'
-    success_url = reverse_lazy('categorias_index')
+# class CategoriaDeleteView(DeleteView):
+#     model = Categoria
+#     template_name = 'administracion/crud/eliminar.html'
+#     success_url = reverse_lazy('categorias_index')
 
-    def get_object(self, queryset=None):
-        pk = self.kwargs.get(self.pk_url_kwarg)
-        obj = get_object_or_404(Categoria, pk=pk)
-        return obj
+#     def get_object(self, queryset=None):
+#         pk = self.kwargs.get(self.pk_url_kwarg)
+#         obj = get_object_or_404(Categoria, pk=pk)
+#         return obj
 
-    # se puede sobreescribir el metodo delete por defecto de la VBC, para que no se realice una baja fisica
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.soft_delete()  # Llamada al método soft_delete() del modelo
-        return HttpResponseRedirect(self.get_success_url())
+#     # se puede sobreescribir el metodo delete por defecto de la VBC, para que no se realice una baja fisica
+#     def delete(self, request, *args, **kwargs):
+#         self.object = self.get_object()
+#         self.object.soft_delete()  # Llamada al método soft_delete() del modelo
+#         return HttpResponseRedirect(self.get_success_url())
