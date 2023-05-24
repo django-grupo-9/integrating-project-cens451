a5e67b4a222273291279b882be996f2088b84032
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-24 12:53:24 -0300

    views de administracion (cambiar a comisiones, campus, etc)

--- administracion/views.py
+++ administracion/views.py
@@ -15,17 +15,14 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 def index_administracion(request):
     variable = 'test variable'
-    return render(request, 'administracion/index_administracion.html', {'variable': variable})
-
-"""
-    CRUD Categorias
-"""
+    return render(request, 'administracion/index_admin.html', {'variable': variable})
 
 
+# CRUD Categorias
 def categorias_index(request):
     # queryset
     categorias = Categoria.objects.filter(baja=False)
-    return render(request,'administracion/categorias/index.html', {'categorias': categorias})
+    return render(request, 'administracion/crud/index.html', {'categorias': categorias})
 
 
 def categorias_nuevo(request):
@@ -36,7 +33,7 @@ def categorias_nuevo(request):
             return redirect('categorias_index')
     else:
         formulario = CategoriaForm()
-    return render(request,'administracion/categorias/nuevo.html', {'form': formulario})
+    return render(request, 'administracion/crud/nuevo.html', {'form': formulario})
 
 
 def categorias_editar(request, id_categoria):
@@ -52,7 +49,7 @@ def categorias_editar(request, id_categoria):
             return redirect('categorias_index')
     else:
         formulario = CategoriaForm(instance=categoria)
-    return render(request, 'administracion/categorias/editar.html', {'form': formulario})
+    return render(request, 'administracion/crud/editar.html', {'form': formulario})
 
 
 def categorias_eliminar(request, id_categoria):
@@ -63,15 +60,12 @@ def categorias_eliminar(request, id_categoria):
     categoria.soft_delete()
     return redirect('categorias_index')
 
-"""
-    IMPLEMENTACION DE CRUD DE CATEGORIA POR MEDIO DE VISTAS BASADAS EN CLASES (VBC)
-"""
 
-
+# IMPLEMENTACION DE CRUD DE CATEGORIA POR MEDIO DE VISTAS BASADAS EN CLASES (VBC)
 class CategoriaListView(ListView):
     model = Categoria
     context_object_name = 'categorias'
-    template_name = 'administracion/categorias/index.html'
+    template_name = 'administracion/crud/index.html'
     queryset = Categoria.objects.filter(baja=False)
     ordering = ['nombre']
 
@@ -80,7 +74,7 @@ class CategoriaCreateView(CreateView):
     model = Categoria
     fields = ['nombre']
     # form_class = CategoriaForm
-    template_name = 'administracion/categorias/nuevo.html'
+    template_name = 'administracion/crud/nuevo.html'
     success_url = reverse_lazy('categorias_index')
 
 
@@ -88,7 +82,7 @@ class CategoriaUpdateView(UpdateView):
     model = Categoria
     fields = ['nombre']
     # form_class = CategoriaForm
-    template_name = 'administracion/categorias/editar.html'
+    template_name = 'administracion/crud/editar.html'
     success_url = reverse_lazy('categorias_index')
 
     def get_object(self, queryset=None):
@@ -99,7 +93,7 @@ class CategoriaUpdateView(UpdateView):
 
 class CategoriaDeleteView(DeleteView):
     model = Categoria
-    template_name = 'administracion/categorias/eliminar.html'
+    template_name = 'administracion/crud/eliminar.html'
     success_url = reverse_lazy('categorias_index')
 
     def get_object(self, queryset=None):
