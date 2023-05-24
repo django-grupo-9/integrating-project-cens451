6165b3fd571ea970aa4c6e9215aefb97c1fe52ce
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-24 13:03:31 -0300

    urls para CRUD orientacion

--- administracion/urls.py
+++ administracion/urls.py
@@ -2,15 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_administracion, name='inicio_administracion'),
-    path('categorias/', views.categorias_index, name='categorias_index'),
-    path('categorias/nuevo/', views.categorias_nuevo, name='categorias_nuevo'),
-    path('categorias/editar/<int:id_categoria>', views.categorias_editar, name='categorias_editar'),
-    path('categorias/eliminar/<int:id_categoria>', views.categorias_eliminar, name='categorias_eliminar'),
+    # path('', views.index_administracion, name='inicio_administracion'),
+    # path('categorias/', views.categorias_index, name='categorias_index'),
+    # path('categorias/nuevo/', views.categorias_nuevo, name='categorias_nuevo'),
+    # path('categorias/editar/<int:id_categoria>', views.categorias_editar, name='categorias_editar'),
+    # path('categorias/eliminar/<int:id_categoria>', views.categorias_eliminar, name='categorias_eliminar'),
 
-    path('categoriasview/', views.CategoriaListView.as_view(), name='categorias_index_view'),
-    path('categorias/viewnuevo', views.CategoriaCreateView.as_view(), name='categorias_nuevo_view'),
-    path('categorias/vieweditar/<int:pk>', views.CategoriaUpdateView.as_view(), name='categorias_editar_view'),
-    path('categorias/vieweliminar/<int:pk>', views.CategoriaDeleteView.as_view(), name='categorias_eliminar_view'),
+    # path('categoriasview/', views.CategoriaListView.as_view(), name='categorias_index_view'),
+    # path('categorias/viewnuevo', views.CategoriaCreateView.as_view(), name='categorias_nuevo_view'),
+    # path('categorias/vieweditar/<int:pk>', views.CategoriaUpdateView.as_view(), name='categorias_editar_view'),
+    # path('categorias/vieweliminar/<int:pk>', views.CategoriaDeleteView.as_view(), name='categorias_eliminar_view'),
+
+
+    path('orentacion/', views.orientacion_index, name='orientacion_index'),
+    path('orientacion/nuevo/', views.orientacion_nuevo, name='orientacion_nuevo'),
+    path('orientacion/editar/<int:id_orientacion>', views.orientacion_editar, name='orientacion_editar'),
+    path('orientacion/eliminar/<int:id_orientacion>', views.orientacion_eliminar, name='orientacion_eliminar'),
 
 ]
