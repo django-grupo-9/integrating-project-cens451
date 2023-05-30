9853a9d429958c32da642f0ba8f9c79f9680efeb
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-30 11:11:56 -0300

    cambio en comision para administracion estudiantes

--- fines/models.py
+++ fines/models.py
@@ -86,7 +86,7 @@ class Comision(models.Model):
         verbose_name_plural = "Comisiones"
 
     def __str__(self):
-        return self.commission
+        return self.commision
 
 
 class Persona(models.Model):
