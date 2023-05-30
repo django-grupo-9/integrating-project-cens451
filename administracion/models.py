fdee8c812908983a26cfdfec1407a87b06b0fbae
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-05-30 11:11:12 -0300

    Cambio en models para administracion

--- administracion/models.py
+++ administracion/models.py
@@ -1,4 +1,4 @@
 from django.db import models
-from fines.models import Orientacion, Asignatura, Comision, Campus
+from fines.models import Orientacion, Asignatura, Comision, Campus, Estudiante
 from cens.models import Administrador, Profesor
 # Create your models here.
